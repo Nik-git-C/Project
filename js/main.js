43f2c1f9730a0af70ab3e0b8daf5753dd224ef39
Nik-git-C <Nik.sivtsov07@gmail.com>
2025-03-26 09:19:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let iframe = document.querySelector('.area-additional')
 console.log(iframe.style);
 
-const element = document.querySelector('.area-main');
-const element2 = document.querySelector('.area-additional');
+const element = document.querySelector('.area-main')
+const element2 = document.querySelector('.area-additional')
 const button = document.querySelector('#id1');
 button.addEventListener('click', () => {
 element.style.width = '99.7VW';
 console.log("Hello world");
 });
 
-const button2 = document.querySelector('#id2');
+const button2 = document.querySelector('#id2')
 button2.addEventListener('click', () => {
         element.style.width = '49VW';
         element2.style.display = 'inline-block';
